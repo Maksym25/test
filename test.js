6a11be87edcb8b74500edf7163572c619b109b8a
Maksym <example@gmail.com>
2019-04-08 01:04:46 +0200

    Primer commit

---
+++ test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var a = 5;
+console.log(a);
+
+let persone={
+    name:"John",
+    age:25,
+    isMarried: false
+}
+
+console.log(persone.isMarried);
+let app = ['dd','2d'];
+console.log(app[1]);
+
+let inc = 10,
+    dec = 10;
+
+inc++;
+dec--;
+
+console.log(inc + " " + dec);
